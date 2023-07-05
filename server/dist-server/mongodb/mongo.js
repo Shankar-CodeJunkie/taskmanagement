1e178181423c737b768e6031ec257c48d543f8d8
Shankar-CodeJunkie <Shankar-CodeJunkie>
2023-07-05 17:15:14 +0530

    fix: new design

---
+++ server/dist-server/mongodb/mongo.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var _mongodb = require("mongodb");
+var url = 'mongodb+srv://dbMongoUser:Password123@cluster0.mk6ie.mongodb.net/?retryWrites=true&w=majority';
+var client = new _mongodb.MongoClient(url);
